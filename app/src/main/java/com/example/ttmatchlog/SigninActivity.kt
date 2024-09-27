fde47bfa242b5cb1591f58dd7e82f19165964f6c
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-09-28 00:37:49 +0900

    update: 新規登録画面のEditTextの入力部分をコンソール出力

--- app/src/main/java/com/example/ttmatchlog/SigninActivity.kt
+++ app/src/main/java/com/example/ttmatchlog/SigninActivity.kt
@@ -2,6 +2,7 @@ package com.example.ttmatchlog
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,10 +19,17 @@ class SigninActivity : AppCompatActivity() {
         binding = ActivitySigninBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 登録画面へ遷移
+        // 登録画面への切り替え
         binding.move.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.signinBtn.setOnClickListener {
+            println("------------")
+            print(binding.userName.getText().toString().trim())
+            print(binding.email.getText().toString().trim())
+            print(binding.password.getText().toString().trim())
         }
     }
 }
