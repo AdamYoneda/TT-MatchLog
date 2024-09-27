27acd58bcf6530f313db910c3a52cc5bb397abb4
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-09-28 06:21:16 +0900

    update: MainViewModelを使用した、サインアウトのボタンを追加。

--- app/src/main/java/com/example/ttmatchlog/view/MainActivity.kt
+++ app/src/main/java/com/example/ttmatchlog/view/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.ttmatchlog.view
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ttmatchlog.databinding.ActivityMainBinding
+import com.example.ttmatchlog.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,5 +17,12 @@ class MainActivity : AppCompatActivity() {
         // bindingの初期化
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // signoutBtnのクリックイベント
+        binding.signoutBtn.setOnClickListener {
+            mainViewModel.signout()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
