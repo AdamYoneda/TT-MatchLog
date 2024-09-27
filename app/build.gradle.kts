ee15a7327e542bd3e1176305977f9eb4a8e201e7
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-09-28 00:55:02 +0900

    feat: プロジェクトレベル、モジュールレベルそれぞれのbuild.gradle.ktsにプラグインやSDKを追加。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,7 @@
 plugins {
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
+
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
 }
@@ -40,7 +43,15 @@ android {
 }
 
 dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
 
+    
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
