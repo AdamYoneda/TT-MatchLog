c292e748d39dff3a8b09cf0ddbdb494d9195dcaa
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-09-27 22:25:11 +0900

    update: 新規登録画面とログイン画面の遷移

--- app/src/main/java/com/example/ttmatchlog/LoginActivity.kt
+++ app/src/main/java/com/example/ttmatchlog/LoginActivity.kt
@@ -1,15 +1,27 @@
 package com.example.ttmatchlog
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.ttmatchlog.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_login)
+        // bindingの初期化
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 登録画面へ遷移
+        binding.move.setOnClickListener {
+            val intent = Intent(this, SigninActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/ttmatchlog/SigninActivity.kt
+++ app/src/main/java/com/example/ttmatchlog/SigninActivity.kt
@@ -1,18 +1,27 @@
 package com.example.ttmatchlog
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.ttmatchlog.databinding.ActivitySigninBinding
 
 class SigninActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySigninBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_signin)
+        // bindingの初期化
+        binding = ActivitySigninBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        // 登録画面へ遷移
+        binding.move.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
-
-
 }
