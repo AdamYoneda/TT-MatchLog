d59953f9777c1458f593d01af53ec58b936827fe
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-09-29 23:22:34 +0900

    add: SigninViewModel - User, UserManagerを使ったコードのリファクタリング。またFirestoreからユーザー情報を取得。

--- app/src/main/java/com/example/ttmatchlog/viewmodel/SigninViewModel.kt
+++ app/src/main/java/com/example/ttmatchlog/viewmodel/SigninViewModel.kt
@@ -1,8 +1,10 @@
 package com.example.myapplication_2.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.ttmatchlog.utils.UserManager
 import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -29,7 +31,15 @@ class SigninViewModel : ViewModel() {
             if (userId != null) {
                 // A-2 新規登録が成功したユーザーをFirestoreへ登録
                 saveUserToFirestore(userId, userName, email) { isSaved ->
-                    _signupResult.value = isSaved // Firestore保存結果を反映
+                    if (isSaved) {
+                        // A-3 Firestoreからユーザー情報を取得
+                        fetchUserInfo(userId) { user ->
+                            // TODO: something
+                            _signupResult.value = true
+                        }
+                    } else {
+                        _signupResult.value = false // Firestoreへの保存失敗
+                    }
                 }
             } else {
                 _signupResult.value = false // ユーザー登録失敗
@@ -37,7 +47,7 @@ class SigninViewModel : ViewModel() {
         }
     }
 
-    // 1.Authを使った新規登録
+    // Authを使った新規登録
     private fun registerUser(email: String, password: String, onResult: (String?) -> Unit) {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
@@ -50,7 +60,7 @@ class SigninViewModel : ViewModel() {
             }
     }
 
-    // 2.新規登録が成功したユーザーをFirestoreへ登録
+    // 新規登録が成功したユーザーをFirestoreへアップロード
     private fun saveUserToFirestore(
         userId: String,
         userName: String,
@@ -70,6 +80,29 @@ class SigninViewModel : ViewModel() {
             }
             .addOnFailureListener {
                 onResult(false)
+            }
+    }
+
+    // Firestoreへアップロードしたユーザー情報を取得
+    private fun fetchUserInfo(userId: String, onResult: (Boolean) -> Unit) {
+        db.collection("users").document(userId).get()
+            .addOnSuccessListener { document ->
+                if (document != null && document.exists()) {
+                    // Firestoreから取得したデータをUserモデルに変換
+                    UserManager.setUser(
+                        userId = document.getString("user_id") ?: "",
+                        userName = document.getString("user_name") ?: "",
+                        email = document.getString("email") ?: "",
+                        joinedDate = document.getTimestamp("joined_date")
+                    )
+                    onResult(true)
+                } else {
+                    // 取得に失敗した場合
+                    Log.d("Firestore", "Failed to fetch user data")
+                }
+            }
+            .addOnFailureListener {
+                Log.d("Firestore", "Failed to fetch user data")
             }
     }
 }
