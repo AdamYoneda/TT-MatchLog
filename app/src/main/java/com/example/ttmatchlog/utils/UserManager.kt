31495e69672e695e03d49331570ae0e6303c9a59
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-09-29 23:13:53 +0900

    add: UserManagerの追加。

---
+++ app/src/main/java/com/example/ttmatchlog/utils/UserManager.kt
@@ -0,0 +1,20 @@
+package com.example.ttmatchlog.utils
+
+import com.example.ttmatchlog.model.User
+import com.google.firebase.Timestamp
+
+object UserManager {
+    private var user: User? = null
+
+    fun setUser(userId: String, userName: String, email: String, joinedDate: Timestamp?) {
+        user = User(userId, userName, email, joinedDate)
+    }
+
+    fun getUser(): User? {
+        return user
+    }
+
+    fun clearUser() {
+        user = null
+    }
+}
