33691f36d8c08f8d6ceb9b416b323141eea0491b
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-10-07 18:33:42 +0900

    add: MatchRecordActivity(.kt,.xml)

---
+++ app/src/main/java/com/example/ttmatchlog/presentation/view/activity/MatchRecordActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ttmatchlog.presentation.view.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ttmatchlog.R
+
+class MatchRecordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_match_record)
+    }
+}
