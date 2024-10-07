73c50994ae550aa73bc371ddc1fd587af4a1aafb
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-10-07 20:27:18 +0900

    add: MatchRecordViewModelFactoryとViewModelの接続。

--- app/src/main/java/com/example/ttmatchlog/presentation/viewmodel/MatchRecordViewModelFactory.kt
+++ app/src/main/java/com/example/ttmatchlog/presentation/viewmodel/MatchRecordViewModelFactory.kt
@@ -1,4 +1,14 @@
 package com.example.ttmatchlog.presentation.viewmodel
 
-class MatchRecordViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.ttmatchlog.data.repository.TournamentRepository
+
+class MatchRecordViewModelFactory(private val repository: TournamentRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MatchRecordViewModel::class.java)) {
+            return MatchRecordViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
