f3bc63cfb8fa9ce283451eb1526739863a6ca605
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-10-11 00:25:24 +0900

    add: TournamentInputActivity

---
+++ app/src/main/java/com/example/ttmatchlog/presentation/view/activity/TournamentInputActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ttmatchlog.presentation.view.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ttmatchlog.R
+
+class TournamentInputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tournament_input)
+    }
+}
