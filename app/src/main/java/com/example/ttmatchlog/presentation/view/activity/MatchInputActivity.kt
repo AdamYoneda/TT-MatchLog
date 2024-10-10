09c83573059c2f3eaef4394d6cdd0379ee7caf38
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-10-11 00:53:13 +0900

    add: MatchInputActivity

---
+++ app/src/main/java/com/example/ttmatchlog/presentation/view/activity/MatchInputActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ttmatchlog.presentation.view.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ttmatchlog.R
+
+class MatchInputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_match_input)
+    }
+}
