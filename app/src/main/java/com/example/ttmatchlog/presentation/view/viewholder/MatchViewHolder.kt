7930a16b86260e17ea6004d02a3571ed8b9248f1
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-10-11 16:50:11 +0900

    refactor: GameScoresのオプションの値を通常のIntに変更。

--- app/src/main/java/com/example/ttmatchlog/presentation/view/viewholder/MatchViewHolder.kt
+++ app/src/main/java/com/example/ttmatchlog/presentation/view/viewholder/MatchViewHolder.kt
@@ -13,25 +13,4 @@ class MatchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         itemView.findViewById<TextView>(R.id.score).text =
             "${match.playerScore} - ${match.opponentScore}"
     }
-
-    private fun formatGameScores(match: Match): String {
-        val gameScores = match.gameScores
-        val scores = mutableListOf<String>()
-        if (gameScores.playerSet1 != null && gameScores.opponentSet1 != null)
-            scores.add("Game1: ${gameScores.playerSet1}-${gameScores.opponentSet1}")
-        if (gameScores.playerSet2 != null && gameScores.opponentSet2 != null)
-            scores.add("Game2: ${gameScores.playerSet2}-${gameScores.opponentSet2}")
-        if (gameScores.playerSet3 != null && gameScores.opponentSet3 != null)
-            scores.add("Game3: ${gameScores.playerSet3}-${gameScores.opponentSet3}")
-        if (gameScores.playerSet4 != null && gameScores.opponentSet4 != null)
-            scores.add("Game4: ${gameScores.playerSet4}-${gameScores.opponentSet4}")
-        if (gameScores.playerSet5 != null && gameScores.opponentSet5 != null)
-            scores.add("Game5: ${gameScores.playerSet5}-${gameScores.opponentSet5}")
-        if (gameScores.playerSet6 != null && gameScores.opponentSet6 != null)
-            scores.add("Game5: ${gameScores.playerSet6}-${gameScores.opponentSet6}")
-        if (gameScores.playerSet7 != null && gameScores.opponentSet7 != null)
-            scores.add("Game7: ${gameScores.playerSet7}-${gameScores.opponentSet7}")
-        // Alternatively, loop through sets
-        return scores.joinToString(", ")
-    }
 }
