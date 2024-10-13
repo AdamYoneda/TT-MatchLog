4407a07b0cd5923439c245355689e91e1ec921e4
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-10-14 00:45:05 +0900

    fix: Tournament, Match, GameScoresの各モデルにデフォルト値を与え、引数なしのコンストラクタを使用できるようになった。（デシリアライズが成功するようになった。）

--- app/src/main/java/com/example/ttmatchlog/data/model/GameScores.kt
+++ app/src/main/java/com/example/ttmatchlog/data/model/GameScores.kt
@@ -2,18 +2,18 @@ package com.example.ttmatchlog.data.model
 
 // 各ゲームセットのスコア (最大7セットをサポート)
 data class GameScores(
-    val playerSet1: Int,
-    val opponentSet1: Int,
-    val playerSet2: Int,
-    val opponentSet2: Int,
-    val playerSet3: Int,
-    val opponentSet3: Int,
-    val playerSet4: Int?,
-    val opponentSet4: Int,
-    val playerSet5: Int,
-    val opponentSet5: Int,
-    val playerSet6: Int,
-    val opponentSet6: Int,
-    val playerSet7: Int,
-    val opponentSet7: Int
+    val playerSet1: Int = 0,
+    val opponentSet1: Int = 0,
+    val playerSet2: Int = 0,
+    val opponentSet2: Int = 0,
+    val playerSet3: Int = 0,
+    val opponentSet3: Int = 0,
+    val playerSet4: Int = 0,
+    val opponentSet4: Int = 0,
+    val playerSet5: Int = 0,
+    val opponentSet5: Int = 0,
+    val playerSet6: Int = 0,
+    val opponentSet6: Int = 0,
+    val playerSet7: Int = 0,
+    val opponentSet7: Int = 0
 )
